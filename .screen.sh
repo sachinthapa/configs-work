eaecc83ebf9b77ec80e0a47e2fe7d28a960e3a90
sachin <sachinthapa@gmail.com>
2021-10-07 18:08:54 +0545

    initialize

---
+++ .screen.sh
@@ -0,0 +1,4 @@
+ #!/bin/sh                                                                                                                   
+ xrandr --newmode "1680x1050"  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
+ xrandr --addmode VGA-1 1680x1050
+ xrandr --output VGA-1 --mode 1680x1050
