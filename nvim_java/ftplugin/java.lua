822ebbc5e4c7e1fc61351440023773cfb1bc0dca
sachin <sachinthapa@gmail.com>
2024-10-16 12:39:07 +0545

    date NA

---
+++ nvim_java/ftplugin/java.lua
@@ -0,0 +1,122 @@
+
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = '/home/sachin/Projects/codewars' .. project_name
+
+
+-- Helper function for creating keymaps
+function nnoremap(rhs, lhs, bufopts, desc)
+  bufopts.desc = desc
+  vim.keymap.set("n", rhs, lhs, bufopts)
+end
+
+-- The on_attach function is used to set key maps after the language server
+-- attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Regular Neovim LSP client keymappings
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  nnoremap('gD', vim.lsp.buf.declaration, bufopts, "Go to declaration")
+  nnoremap('gd', vim.lsp.buf.definition, bufopts, "Go to definition")
+  nnoremap('gi', vim.lsp.buf.implementation, bufopts, "Go to implementation")
+  nnoremap('K', vim.lsp.buf.hover, bufopts, "Hover text")
+  nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts, "Show signature")
+  nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, bufopts, "Add workspace folder")
+  nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts, "Remove workspace folder")
+  nnoremap('<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts, "List workspace folders")
+  nnoremap('<space>D', vim.lsp.buf.type_definition, bufopts, "Go to type definition")
+  nnoremap('<space>rn', vim.lsp.buf.rename, bufopts, "Rename")
+  nnoremap('<space>ca', vim.lsp.buf.code_action, bufopts, "Code actions")
+  vim.keymap.set('v', "<space>ca", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
+    { noremap=true, silent=true, buffer=bufnr, desc = "Code actions" })
+  nnoremap('<space>f', function() vim.lsp.buf.format { async = true } end, bufopts, "Format file")
+
+  -- Java extensions provided by jdtls
+  nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
+  nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
+  nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
+  vim.keymap.set('v', "<space>em", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]],
+    { noremap=true, silent=true, buffer=bufnr, desc = "Extract method" })
+end
+
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+  flags = {
+     debounce_text_changes = 80,
+   },
+  on_attach = on_attach,  -- We pass our on_attach keybindings to the configuration map
+  -- 💀
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- These are defined by the eclipse.jdt.ls project and will be passed to eclipse when starting.
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    -- 💀
+    '/usr/lib/jvm/java-17-openjdk/bin/java',
+            -- or '/path/to/java17_or_newer/bin/java'
+            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    -- 💀
+    '-jar', '/home/sachin/Downloads/Jdt/plugins/org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar',
+         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+         -- Must point to the                                                     Change this to
+         -- eclipse.jdt.ls installation                                           the actual version
+
+
+    -- 💀
+    '-configuration', '/home/sachin/Downloads/Jdt/config_linux',
+                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                    -- eclipse.jdt.ls installation            Depending on your system.
+
+
+    -- 💀
+    -- See `data directory configuration` section in the README
+    '-data', workspace_dir,
+  },
+
+  -- capabilities = capabilities
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  -- settings = {
+  --   java = {
+  --   }
+  -- },
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  -- init_options = {
+  --   bundles = {}
+  -- },
+}
+
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require('jdtls').start_or_attach(config)
